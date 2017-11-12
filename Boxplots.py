3d86ff8609f3510970f78aa5afcfd6b114930e89
Rohan Mukesh Kadakia <kadakia@m-kadakia01.local>
2017-11-11 21:27:46 -0500

    a

--- Boxplots.py
+++ Boxplots.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[127]:
+# In[87]:
 
 from bokeh.io import curdoc, output_file, show, output_notebook
 from bokeh.plotting import figure, ColumnDataSource
@@ -11,8 +11,6 @@ from bokeh.palettes import Spectral6
 # from bokeh.charts import BoxPlot, Histogram
 # import holoviews as hv
 # hv.extension('bokeh')
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 
 # from bokeh.server.server import Server
 # from bokeh.embed import autoload_server
@@ -37,11 +35,10 @@ print(data.info())
 # data_Sou = data[data['region'] == 'South Asia']
 
 
-# In[187]:
+# In[121]:
 
 import numpy as np
-# from math import pi
-
+#from math import pi
 
 
 # In[119]:
@@ -91,6 +88,318 @@ import numpy as np
 # plt.show()
 
 
+# In[122]:
+
+# Make a slider object
+slider_2 = Slider(start = 1970, end = 1980, step = 10, value = 1970, title = 'Year')
+
+# find the quartiles and IQR for each category
+groups = data.loc[1970].groupby('region')
+q1 = groups.quantile(q=0.25)
+q2 = groups.quantile(q=0.5)
+q3 = groups.quantile(q=0.75)
+iqr = q3 - q1
+upper = q3 + 1.5*iqr
+lower = q1 - 1.5*iqr
+
+# find the outliers for each category
+def outliers(group):
+    cat = group.name
+    return group[(group.life > upper.loc[cat]['life']) | (group.life < lower.loc[cat]['life'])]['life']
+out = groups.apply(outliers).dropna()
+
+# prepare outlier data for plotting, we need coordinates for every outlier.
+if not out.empty:
+    outx = []
+    outy = []
+    for cat in list(data['region'].unique()):
+        # only add outliers if they exist
+        if not out.loc[cat].empty:
+            for value in out[cat]:
+                outx.append(cat)
+                outy.append(value)
+                
+p = figure(background_fill_color="#EFE8E2", title="", x_range=list(data['region'].unique()))
+p.xaxis.major_label_orientation = pi/2
+
+# if no outliers, shrink lengths of stems to be no longer than the minimums or maximums
+qmin = groups.quantile(q=0.00)
+qmax = groups.quantile(q=1.00)
+upper.life = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'life']),upper.life)]
+lower.life = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'life']),lower.life)]
+
+# stems
+p.segment(list(data['region'].unique()), upper.life, list(data['region'].unique()), q3.life, line_color="black")
+p.segment(list(data['region'].unique()), lower.life, list(data['region'].unique()), q1.life, line_color="black")
+
+# boxes
+p.vbar(list(data['region'].unique()), 0.7, q2.life, q3.life, fill_color="#E08E79", line_color="black")
+p.vbar(list(data['region'].unique()), 0.7, q1.life, q2.life, fill_color="#3B8686", line_color="black")
+
+# whiskers (almost-0 height rects simpler than segments)
+p.rect(list(data['region'].unique()), lower.life, 0.2, 0.01, line_color="black")
+p.rect(list(data['region'].unique()), upper.life, 0.2, 0.01, line_color="black")
+
+# outliers
+# if not out.empty:
+#     p.circle(outx, outy, size=6, color="#F38630", fill_alpha=0.6)
+
+p.xgrid.grid_line_color = None
+p.ygrid.grid_line_color = "white"
+p.grid.grid_line_width = 2
+p.xaxis.major_label_text_font_size="10pt"
+
+# Define the callback function
+def update_plot(attr, old, new):
+    if slider_2.value == 1970:
+        # find the quartiles and IQR for each category
+        groups = data.loc[1970].groupby('region')
+        q1 = groups.quantile(q=0.25)
+        q2 = groups.quantile(q=0.5)
+        q3 = groups.quantile(q=0.75)
+        iqr = q3 - q1
+        upper = q3 + 1.5*iqr
+        lower = q1 - 1.5*iqr
+
+        # find the outliers for each category
+        def outliers(group):
+            cat = group.name
+            return group[(group.life > upper.loc[cat]['life']) | (group.life < lower.loc[cat]['life'])]['life']
+        out = groups.apply(outliers).dropna()
+
+        # prepare outlier data for plotting, we need coordinates for every outlier.
+        if not out.empty:
+            outx = []
+            outy = []
+            for cat in list(data['region'].unique()):
+                # only add outliers if they exist
+                if not out.loc[cat].empty:
+                    for value in out[cat]:
+                        outx.append(cat)
+                        outy.append(value)
+  
+        p = figure(background_fill_color="#EFE8E2", title="", x_range=list(data['region'].unique()))
+        p.xaxis.major_label_orientation = pi/2
+
+        # if no outliers, shrink lengths of stems to be no longer than the minimums or maximums
+        qmin = groups.quantile(q=0.00)
+        qmax = groups.quantile(q=1.00)
+        upper.life = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'life']),upper.life)]
+        lower.life = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'life']),lower.life)]
+
+        # stems
+        p.segment(list(data['region'].unique()), upper.life, list(data['region'].unique()), q3.life, line_color="black")
+        p.segment(list(data['region'].unique()), lower.life, list(data['region'].unique()), q1.life, line_color="black")
+
+        # boxes
+        p.vbar(list(data['region'].unique()), 0.7, q2.life, q3.life, fill_color="#E08E79", line_color="black")
+        p.vbar(list(data['region'].unique()), 0.7, q1.life, q2.life, fill_color="#3B8686", line_color="black")
+
+        # whiskers (almost-0 height rects simpler than segments)
+        p.rect(list(data['region'].unique()), lower.life, 0.2, 0.01, line_color="black")
+        p.rect(list(data['region'].unique()), upper.life, 0.2, 0.01, line_color="black")
+
+        # outliers
+        # if not out.empty:
+        #     p.circle(outx, outy, size=6, color="#F38630", fill_alpha=0.6)
+
+        p.xgrid.grid_line_color = None
+        p.ygrid.grid_line_color = "white"
+        p.grid.grid_line_width = 2
+        p.xaxis.major_label_text_font_size="10pt"
+    elif slider_2.value == 1980:
+        # find the quartiles and IQR for each category
+        groups = data.loc[1980].groupby('region')
+        q1 = groups.quantile(q=0.25)
+        q2 = groups.quantile(q=0.5)
+        q3 = groups.quantile(q=0.75)
+        iqr = q3 - q1
+        upper = q3 + 1.5*iqr
+        lower = q1 - 1.5*iqr
+
+        # find the outliers for each category
+        def outliers(group):
+            cat = group.name
+            return group[(group.life > upper.loc[cat]['life']) | (group.life < lower.loc[cat]['life'])]['life']
+        out = groups.apply(outliers).dropna()
+
+        # prepare outlier data for plotting, we need coordinates for every outlier.
+        if not out.empty:
+            outx = []
+            outy = []
+            for cat in list(data['region'].unique()):
+                # only add outliers if they exist
+                if not out.loc[cat].empty:
+                    for value in out[cat]:
+                        outx.append(cat)
+                        outy.append(value)
+  
+        p = figure(background_fill_color="#EFE8E2", title="", x_range=list(data['region'].unique()))
+        p.xaxis.major_label_orientation = pi/2
+
+        # if no outliers, shrink lengths of stems to be no longer than the minimums or maximums
+        qmin = groups.quantile(q=0.00)
+        qmax = groups.quantile(q=1.00)
+        upper.life = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'life']),upper.life)]
+        lower.life = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'life']),lower.life)]
+
+        # stems
+        p.segment(list(data['region'].unique()), upper.life, list(data['region'].unique()), q3.life, line_color="black")
+        p.segment(list(data['region'].unique()), lower.life, list(data['region'].unique()), q1.life, line_color="black")
+
+        # boxes
+        p.vbar(list(data['region'].unique()), 0.7, q2.life, q3.life, fill_color="#E08E79", line_color="black")
+        p.vbar(list(data['region'].unique()), 0.7, q1.life, q2.life, fill_color="#3B8686", line_color="black")
+
+        # whiskers (almost-0 height rects simpler than segments)
+        p.rect(list(data['region'].unique()), lower.life, 0.2, 0.01, line_color="black")
+        p.rect(list(data['region'].unique()), upper.life, 0.2, 0.01, line_color="black")
+
+        # outliers
+        # if not out.empty:
+        #     p.circle(outx, outy, size=6, color="#F38630", fill_alpha=0.6)
+
+        p.xgrid.grid_line_color = None
+        p.ygrid.grid_line_color = "white"
+        p.grid.grid_line_width = 2
+        p.xaxis.major_label_text_font_size="10pt"
+        
+    
+# Attach the callback to the 'value' property of slider
+slider_2.on_change('value', update_plot)
+
+#layout = row(widgetbox(slider_2), p)
+
+
+
+# Make the ColumnDataSource
+source = ColumnDataSource(data={
+                          'x'       : data.loc[1970].fertility,
+                          'y'       : data.loc[1970].life,
+                          'country'      : data.loc[1970].Country,
+                          #    'pop'      : (data.loc[1970].population / 20000000) + 2,
+                          'region'      : data.loc[1970].region
+                          })
+
+# Save the minimum and maximum values of the fertility column
+xmin, xmax = min(data.fertility), max(data.fertility)
+
+# Save the minimum and maximum values of the life expectancy column
+ymin, ymax = min(data.life), max(data.life)
+
+# Create the figure
+plot = figure(title='Gapminder Data for 1970', plot_height=400, plot_width=700,
+              x_range=(xmin, xmax), y_range=(ymin, ymax))
+
+# Add circle glyphs to the plot
+# plot.circle(x='x', y='y', fill_alpha=0.8, source=source_2)
+
+# Set the x-axis label
+plot.xaxis.axis_label ='fertility'
+
+# Set the y-axis label
+plot.yaxis.axis_label = 'life'
+
+# Add the plot to the current document and add a title
+# curdoc().add_root(plot)
+# curdoc().title = 'Gapminder'
+
+# output_file('gapminder_2.html')
+# show(plot)
+
+# Make a list of the unique values from the region column
+regions_list = data.region.unique().tolist()
+
+# Make a color mapper
+color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
+
+# output_file('gapminder_2.html')
+# show(plot)
+
+# Make a slider object
+slider = Slider(start = 1970, end = 2010, step = 1, value = 1970, title = 'Year')
+
+# Define the callback function
+def update_plot_2(attr, old, new):
+    
+    yr = slider.value
+    x = x_select.value
+    y = y_select.value
+    # Label axes of plot
+    plot.xaxis.axis_label = x
+    plot.yaxis.axis_label = y
+    
+    new_data = {
+        'x'       : data.loc[yr][x],
+        'y'       : data.loc[yr][y],
+        'country' : data.loc[yr].Country,
+        #        'pop'     : (data.loc[yr].population / 20000000) + 2,
+        'region'  : data.loc[yr].region
+    }
+    source.data = new_data
+
+# Set the range of all axes
+plot.x_range.start = min(data[x])
+    plot.x_range.end = max(data[x])
+    plot.y_range.start = min(data[y])
+    plot.y_range.end = max(data[y])
+    
+    # Add title to figure
+    plot.title.text = 'Gapminder data for %d' % yr
+
+# Add the color mapper to the circle glyph
+plot.circle(x='x', y='y', fill_alpha=0.8, source=source,
+            color=dict(field='region', transform=color_mapper), legend='region')
+
+# Set the legend.location attribute of the plot
+plot.legend.location = 'bottom_left'
+
+# Attach the callback to the 'value' property of slider
+slider.on_change('value',update_plot_2)
+
+# Create a HoverTool
+hover = HoverTool(tooltips = [('Country', '@country')])
+
+# Add the HoverTool to the plot
+plot.add_tools(hover)
+
+# Create a dropdown Select widget for the x data
+x_select = Select(
+                  options=['fertility', 'life', 'child_mortality', 'gdp'],
+                  value='fertility',
+                  title='x-axis data'
+                  )
+
+# Attach the update_plot callback to the 'value' property of x_select
+x_select.on_change('value', update_plot_2)
+
+# Create a dropdown Select widget for the y data
+y_select = Select(
+                  options=['fertility', 'life', 'child_mortality'],
+                  value='life',
+                  title='y-axis data'
+                  )
+
+# Attach the update_plot callback to the 'value' property of y_select
+y_select.on_change('value', update_plot_2)
+
+#layout = row(widgetbox(slider,x_select,y_select), plot)
+
+# output_file('gapminder.html')
+# show(layout)
+#curdoc().add_root(layout)
+
+
+tab1 = Panel(child=row(widgetbox(slider,x_select,y_select), plot), title='Interactive Scatter')
+
+tab2 = Panel(child=row(widgetbox(slider_2), p), title='Box Plots')
+
+layout = Tabs(tabs=[tab1, tab2])
+
+
+curdoc().add_root(layout)
+output_notebook()
+show(layout)
 
 
 # Define the callback function
@@ -149,202 +458,6 @@ import numpy as np
 # curdoc().add_root(layout)
 # output_notebook()
 # show(layout)
-
-
-# In[199]:
-
-groups = data.loc[1970].groupby('region')
-
-q1 = groups.quantile(q=0.25)
-q2 = groups.quantile(q=0.5)
-q3 = groups.quantile(q=0.75)
-iqr = q3 - q1
-upper = q3 + 1.5*iqr
-lower = q1 - 1.5*iqr
-
-# Make the ColumnDataSource
-source_2 = ColumnDataSource(data={
-                          'x'       : data.loc[1970].region,
-                          'y'       : data.loc[1970].life
-                          })
-
-    
-p = figure(background_fill_color="#EFE8E2", title="Boxplots by region, 1970", x_range=sorted(list(data['region'].unique())))
-p.xaxis.major_label_orientation = np.pi/2
-
-# if no outliers, shrink lengths of stems to be no longer than the minimums or maximums
-qmin = groups.quantile(q=0.00)
-qmax = groups.quantile(q=1.00)
-upper.life = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'life']),upper.life)]
-lower.life = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'life']),lower.life)]
-#p.y_range.start = min(data['life'])
-#p.y_range.end = max(data['life'])
-
-p.xgrid.grid_line_color = None
-p.ygrid.grid_line_color = "white"
-p.grid.grid_line_width = 2
-p.xaxis.major_label_text_font_size="10pt"
-
-p.xaxis.axis_label = 'region'
-p.yaxis.axis_label = 'life'
-
-
-# Make a slider object
-slider_2 = Slider(start = 1970, end = 2010, step = 1, value = 1970, title = 'Year')
-
-# Create a dropdown Select widget for the x data
-x_select = Select(
-                  options=['region'],
-                  value='region',
-                  title='x-axis data'
-                  )
-
-
-
-# Create a dropdown Select widget for the y data
-y_select = Select(
-                  options=['fertility', 'life', 'child_mortality', 'gdp'],
-                  value='life',
-                  title='y-axis data'
-                  )
-
-if y_select.value == 'life':
-    upper_y = upper['life']
-elif y_select.value == 'fertility':
-    upper_y = upper['fertility']
-elif y_select.value == 'child_mortality':
-    upper_y = upper['child_mortality']
-elif y_select.value == 'gdp':
-    upper_y = upper['gdp']
-    
-if y_select.value == 'life':
-    lower_y = lower['life']
-elif y_select.value == 'fertility':
-    lower_y = lower['fertility']
-elif y_select.value == 'child_mortality':
-    lower_y = lower['child_mortality']
-elif y_select.value == 'gdp':
-    lower_y = lower['gdp']
-    
-if y_select.value == 'life':
-    q1_y = q1['life']
-elif y_select.value == 'fertility':
-    q1_y = q1['fertility']
-elif y_select.value == 'child_mortality':
-    q1_y = q1['child_mortality']
-elif y_select.value == 'gdp':
-    q1_y = q1['gdp']
-    
-if y_select.value == 'life':
-    q2_y = q2['life']
-elif y_select.value == 'fertility':
-    q2_y = q2['fertility']
-elif y_select.value == 'child_mortality':
-    q2_y = q2['child_mortality']
-elif y_select.value == 'gdp':
-    q2_y = q2['gdp']
-    
-if y_select.value == 'life':
-    q3_y = q3['life']
-elif y_select.value == 'fertility':
-    q3_y = q3['fertility']
-elif y_select.value == 'child_mortality':
-    q3_y = q3['child_mortality']
-elif y_select.value == 'gdp':
-    q3_y = q3['gdp']
-
-
-# Define the callback function
-def update_plot(attr, old, new):
-    
-    yr = slider_2.value
-    # find the quartiles and IQR for each category
-    groups = data.loc[yr].groupby('region')
-    x = x_select.value
-    y = y_select.value
-    
-    # Label axes of plot
-#    p.xaxis.axis_label = x
-    p.yaxis.axis_label = y
-
-    new_data = {
-        'x'       : data.loc[yr][x],
-        'y'       : data.loc[yr][y]
-    }
-    source_2.data = new_data
-
-    # Set the range of all axes
-#    p.x_range.start = min(data[x])
-#    p.x_range.end = max(data[x])
-    p.y_range.start = min(data[y])
-    p.y_range.end = max(data[y])
-    #qmin = groups.quantile(q=0.00)
-    #qmax = groups.quantile(q=1.00)
-    #upper[y] = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,y]),upper[y])]
-    #lower[y] = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,y]),lower[y])]
-    
-    # Add title to figure
-    p.title.text = 'Boxplots by region, for %d' % yr
-    
-
-# Attach the update_plot callback to the 'value' property of y_select
-y_select.on_change('value', update_plot)
-
-if x_select.value == 'region':
-    x_6 = sorted(list(data['region'].unique()))
-
-
-# find the outliers for each category
-#def outliers(group):
-#    cat = group.name
-#    if y_select.value == 'life':
-#        group_y = group['life']
-#    elif y_select.value == 'fertility':
-#        group_y = group['fertility']
-#    elif y_select.value == 'child_mortality':
-#        group_y = group['child_mortality']
-#    elif y_select.value == 'gdp':
-#        group_y = group['gdp']
-#    return group[(group['y'] > upper.loc[cat]['y']) | (group['y'] < lower.loc[cat]['y'])]['y']
-#out = groups.apply(outliers).dropna()
-
-# prepare outlier data for plotting, we need coordinates for every outlier.
-#if not out.empty:
-#    outx = []
-#    outy = []
-#    for cat in sorted(list(data['region'].unique())):
-        # only add outliers if they exist
-#        if not out.loc[cat].empty:
-#            for value in out[cat]:
-#                outx.append(cat)
-#                outy.append(value)
-
-# stems
-p.segment(x_6, upper_y, x_6, q3_y, line_color="black")
-p.segment(x_6, lower_y, x_6, q1_y, line_color="black")
-
-# boxes
-p.vbar(x_6, 0.7, q2_y, q3_y, fill_color="#E08E79", line_color="black")
-p.vbar(x_6, 0.7, q1_y, q2_y, fill_color="#3B8686", line_color="black")
-
-# whiskers (almost-0 height rects simpler than segments)
-p.rect(x_6, upper_y, width = 0.2, height = 0.01, line_color="black", source = source_2)
-p.rect(x_6, lower_y, width = 0.2, height = 0.01, line_color="black")
-
-# outliers
-#if not out.empty:
-#    p.circle(outx, outy, size=6, color="#F38630", fill_alpha=0.6)
-    
-
-# Attach the callback to the 'value' property of slider
-slider_2.on_change('value', update_plot)
-
-
-layout = row(widgetbox(slider,y_select), p)
-
-curdoc().add_root(layout)
-output_notebook()
-show(layout)
 
 
 # In[120]:
